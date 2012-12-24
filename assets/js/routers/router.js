3a7e42b985bb9d341998f3e5a32780cbd8a54701
Fazle Taher <ftaher@gmail.com>
2012-12-24 22:51:45 +0600

    topic uploads

--- assets/js/routers/router.js
+++ assets/js/routers/router.js
@@ -1,39 +1,43 @@
 App.RubyConfRouter = Backbone.Router.extend({
-    routes:{
-        "": "index",
-        "sponsor": "sponsor",
-        "topic": "topic",
-        "speaker": "speaker",
-        "about": "about",
-        "proposal": "proposal",
-        "register": "register"
+  routes:{
+    "":"index",
+    "sponsor":"sponsor",
+    "topic":"topic",
+    "speaker":"speaker",
+    "about":"about",
+    "proposal":"proposal",
+    "register":"register"
 
-    },
-    start:function () {
-        Backbone.history.start();
-    },
-    index: function(actions){
-        var indexView = new App.IndexView;
-        indexView.render();
-    },
-    sponsor: function(actions){
-        var view = new App.SponsorView;
-        view.render();
-    },
-    speaker: function(actions){
-        var view = new App.SpeakerView;
-        view.render();
-    },
-    about: function(actions){
-        var view = new App.AboutView;
-        view.render();
-    },
-    proposal: function(actions){
-        var view = new App.ProposalView;
-        view.render();
-    },
-    register: function(actions){
-        var view = new App.RegisterView;
-        view.render();
-    }
+  },
+  start:function () {
+    Backbone.history.start();
+  },
+  index:function (actions) {
+    var indexView = new App.IndexView;
+    indexView.render();
+  },
+  sponsor:function (actions) {
+    var view = new App.SponsorView;
+    view.render();
+  },
+  speaker:function (actions) {
+    var view = new App.SpeakerView;
+    view.render();
+  },
+  about:function (actions) {
+    var view = new App.AboutView;
+    view.render();
+  },
+  proposal:function (actions) {
+    var view = new App.ProposalView;
+    view.render();
+  },
+  register:function (actions) {
+    var view = new App.RegisterView;
+    view.render();
+  },
+  topic:function (actions) {
+    var view = new App.TopicView;
+    view.render();
+  }
 });
